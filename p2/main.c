c13d370eec8f3957f6c5d604009a24fab1c14cb4
2024125087 <yodst0127@kau.kr>
2026-06-01 16:19:46 +0900

    Improve code structure and readability in main.c
    Refactor loops for better readability and structure.

--- p2/main.c
+++ p2/main.c
@@ -12,15 +12,21 @@ int main(void)
     int *p2 = nullptr; 
 
     for (int *p1 = A; p1 < A + N; p1++)
+    {
         scanf("%d", p1);
-
-    for (int *p2 = B; p2 < B + N; p2++)
-        scanf("%d", p2);
-
-    for (p1 = A ,p2 = B + N -1; p1 < A + N; p1++, p2--)
-        printf("%d ", *p1 + *p2);
+    }
     
+    for (int *p2 = B; p2 < B + N; p2++)
+    {
+        scanf("%d", p2);
+    }
+    
+    for (p1 = A ,p2 = B + N -1; p1 < A + N; p1++, p2--)
+    {
+        printf("%d ", *p1 + *p2);
+    }    
 
     printf("\n");
+    
     return 0;
 }
