86a604f44c1a7d7f6b74c846f45cf09893b3d976
2024125087 <yodst0127@kau.kr>
2026-06-01 15:39:29 +0900

    Refactor main function to read characters and count
    Refactored the main function to read characters individually and updated the logic for counting occurrences.

--- p1/main.c
+++ p1/main.c
@@ -2,29 +2,36 @@
 
 int main(void) 
 {
-    char x[10] = {}; // 배열선언, 초기화
-    
-    scanf("%s", x); // 영문자를 입력받아 배열 x에 저장
+    char x[10] = {};
+    char *px = x;               
 
-    char maxChar = x[0]; // 변수 선언, 초기화
-    int  maxCount = 0;  // 변수 선언, 초기화
+    for (char *p = px; p < px + 10; p++)    
+        scanf("%c", p);
     
-    for (char *p = x; p < x + 10; p++) 
+    char maxChar = *px;        
+    int  maxCount = 0;          
+    
+    for (char *p = px; p < px + 10; p++)       
     {
         int count = 0;
         
-        for (char *q = x; q < x + 10; q++) 
+        for (char *q = px; q < px + 10; q++)  
         {
             if (*p == *q)
                 count++;
         }
         
-        if (count > maxCount) 
+        if (count > maxCount)   // count가 maxCount 초과할 때만 갱신
         {
             maxCount = count;
             maxChar  = *p;
         }
     }
+    
+    printf("%c %d\n", maxChar, maxCount);
+    
+    return 0;
+}
     
     printf("%c %d\n", maxChar, maxCount);
     
